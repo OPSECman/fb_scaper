28e12ffe1a29f8fa6030464e75156297b38d3b13
OPSECman <merrydeceiver@tutanota.com>
2016-12-17 19:40:41 -0600

    Update fb_scaper.py

--- fb_scaper.py
+++ fb_scaper.py
@@ -13,7 +13,7 @@ def main():
 	page = sys.argv[1]
 	access_token = "YOUR ACCESS TOKEN GOES HERE"
 	graph = facepy.GraphAPI(access_token)
-	url_comments_likes = page + "/posts?limit=100&fields=likes{id,name},comments{id}"
+	url_comments_likes = page + "/posts?limit=250&fields=likes{id,name},comments"
 	
 	comments_likes = graph.get(url_comments_likes)
 	for key, value in comments_likes.iteritems():
@@ -21,7 +21,7 @@ def main():
 			first_list = value
 			for i in first_list:
 				for key, value in i.iteritems():
-					if key == 'likes' or key == 'comments':
+					if key == 'likes':
 						temp_dict = value
 						for key, value in temp_dict.iteritems():
 							if key == 'data':
@@ -30,10 +30,26 @@ def main():
 									for key, value in j.iteritems():
 										if key == 'id' and '_' not in value:
 											print "id:\t"+value
-										if key == 'name':
+										elif key == 'name':
 											print "name:\t"+value+"\r\n"
+					elif key == 'comments':
+						temp_dict2 = value
+						for key, value in temp_dict2.iteritems():
+							if key == 'data':
+								third_list = value
+								for k in third_list:
+									for key, value in k.iteritems():
+										if key == 'from':
+											temp_dict3 = value
+											for key, value in temp_dict3.iteritems():
+												if key == 'name':
+													print "name:\t"+value
+												if key == 'id' and '_' not in value:
+													print "id:\t"+value+"\r\n"
+												
 
 	return
 
 if __name__ == "__main__":
 	main()
+
